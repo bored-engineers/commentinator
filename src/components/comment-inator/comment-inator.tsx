85aa1ba826ec9ad40e303e7ab3101d8e232afbb9
Ajit Singh <as.ajitsingh@outlook.com>
2023-04-08 22:57:21 +0530

    Adds Config interface and refactoring

--- src/components/comment-inator/comment-inator.tsx
+++ src/components/comment-inator/comment-inator.tsx
@@ -2,33 +2,34 @@ import { Component, getAssetPath, h, Host, Prop, State } from '@stencil/core';
 import firebase from 'firebase/compat/app';
 import { getAuth, signInWithPopup, GithubAuthProvider, setPersistence, browserLocalPersistence, signOut, User } from 'firebase/auth';
 import { getFirestore, getDocs, collection, query, where, doc, setDoc, orderBy } from 'firebase/firestore';
-import { Comment } from './types';
+import { IComment, IConfig } from './types';
 import { getGithubUser } from './github-service';
 import { v4 as uuid } from 'uuid';
 import formatTimeAgo from './utils/time.util';
 
-declare var firebaseConfig: any;
+declare var config: IConfig;
 
 @Component({
   tag: 'comment-inator',
   styleUrl: 'comment-inator.css',
   assetsDirs: ['assets'],
 })
+
 export class CommentInator {
-  firebaseApp: firebase.app.App = firebase.initializeApp(firebaseConfig);
+  firebaseApp: firebase.app.App = firebase.initializeApp(config.firebase);
   auth = getAuth(this.firebaseApp);
   firestore = getFirestore(this.firebaseApp);
 
   @State() showLoader: boolean = true;
   @State() user: User & { username: string };
-  @State() comments: Comment[] = [];
+  @State() comments: IComment[] = [];
   @State() currentCommentText = '';
 
   @Prop() height: string;
   @Prop() groupId: string;
 
   fetchAllComments = async () => {
-    const querySnapshot = await getDocs(query(collection(this.firestore, 'blog-comments'), where('groupId', '==', this.groupId), orderBy('createdAt', 'desc')));
+    const querySnapshot = await getDocs(query(collection(this.firestore, config.collectionName), where('groupId', '==', this.groupId), orderBy('createdAt', 'desc')));
     this.comments = await Promise.all(
       querySnapshot.docs.map(async doc => {
         const comment = doc.data();
@@ -48,7 +49,7 @@ export class CommentInator {
 
   onPostCommentClickHandler = async () => {
     if (!this.currentCommentText) return;
-    await setDoc(doc(this.firestore, 'blog-comments', uuid()), {
+    await setDoc(doc(this.firestore, config.collectionName, uuid()), {
       text: this.currentCommentText,
       groupId: this.groupId,
       githubId: this.user.providerData[0].uid,
@@ -64,20 +65,15 @@ export class CommentInator {
       .catch(e => console.error(e));
   };
 
-  onLogoutClickHandler = () => {
-    signOut(this.auth)
-      .then(result => console.log(result))
-      .catch(e => console.error(e));
-  };
+  onLogoutClickHandler = () => signOut(this.auth);
 
   onInputHandler = e => {
     this.currentCommentText = e.target.value;
   };
 
   render() {
-    console.log('rendering');
     return (
-      <Host>
+      <Host class="commentinator">
         <div class="comment-form">
           {!this.user && (
             <div class="login">
--- src/components/comment-inator/types.ts
+++ src/components/comment-inator/types.ts
@@ -1,8 +1,17 @@
-export type Comment = {
+export type IComment = {
   text: string;
   username: string;
   name: string;
   imageUrl: string;
   id: string;
-  createdAt: Date
+  createdAt: Date;
+};
+
+export type IConfig = {
+  collectionName: string;
+  firebase: {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+  };
 };
